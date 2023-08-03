2fefa806c1e5fd1aea6e154e10f45adc986ffc4b
ZaidAhmed27 <zaidahmedmohd27@gmail.com>
2023-08-03 16:43:54 -0400

    Add project files.

---
+++ CarDeals/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarDeals.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
